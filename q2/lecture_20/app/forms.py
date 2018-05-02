ce35a522023efe465234ffeb94a07caa823d1ef6
cwong2015 <sirhc8002@gmail.com>
2018-05-02 04:40:15 -0400

    finished making web calculator using flask

--- q2/lecture_20/app/forms.py
+++ q2/lecture_20/app/forms.py
@@ -7,32 +7,6 @@ from app.models import User
 class SubmissionForm(FlaskForm):
     number = FloatField('First Number:', validators=[DataRequired()])
     number2 = FloatField('Second Number:', validators=[DataRequired()])
-    operator = RadioField('Operator:', choices =[('choice1', 'Add'), ('choice2','Subtract'),
-                                                  ('choice3', 'Multiply'), ('choice4', 'Divide')])
+    operator = RadioField('Operator:', choices =[('add', 'Add'), ('subtract','Subtract'),
+                                                  ('multiply', 'Multiply'), ('divide', 'Divide')])
     submit = SubmitField('Submit Entry')
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
--- q2/lecture_20/app/routes.py
+++ q2/lecture_20/app/routes.py
@@ -1,81 +1,41 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import SubmissionForm, LoginForm, RegistrationForm
-from app.models import Post, User
-from app import db
-from flask_login import login_user, logout_user, current_user, login_required
+from app.forms import SubmissionForm
+from app.models import Post
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SubmissionForm()
-    return render_template('index.html', title='Home', form=form)
-    # if form.validate_on_submit():
-    #     num = form.number.data
-    #     num2 = form.number2.data
-    #     op = form.operator.data
-    #     return redirect(url_for('index'))
+    if request.method=='POST':
+        selectedValue = request.form['operator']
+        new_num1 = request.form['number']
+        new_num2 = request.form['number2']
+        if form.validate()== False:
+            return render_template('404.html')
+        else:
+            return redirect(url_for('result', selectedValue=selectedValue, num1=new_num1, num2=new_num2))
 
-# @app.route('/posts/<postid>')
-# def showpost(postid):
-#     if current_user.is_authenticated:
-#         current_posts = Post.query.filter_by(id=postid)
-#         return render_template('showpost.html', title='Show Post', post=current_posts[0])
-#     return render_template('notloggedin.html')
+    elif request.method=='GET':
+        return render_template('index.html', title='Home', form=form)
+    
+@app.route('/result/<selectedValue>')
+def result(selectedValue):
+    num1=float(request.args.get('num1'))
+    num2=float(request.args.get('num2'))
 
+    if selectedValue == 'subtract':
+        result=num1-num2
+    elif selectedValue == 'multiply':
+        result=num1*num2
+    elif selectedValue == 'add':
+        result=num1+num2
+    else:
+        result=num1/num2
+    
+    if((str(result))[-2:] == '.0'):
+        result = str(result)
+        result=result[:-2]
 
-# @app.route('/submit', methods=['GET', 'POST'])
-# def submitpage():
-#     if current_user.is_authenticated:
-#         form = SubmissionForm()
-#         if form.validate_on_submit():
-#             flash('Topic {} has been submitted.'.format(
-#                 form.title.data))
-#             new_title = form.title.data
-#             new_text = form.text.data
-#             p = Post(title=new_title,body=new_text)
-#             db.session.add(p)
-#             db.session.commit()
-#             return redirect(url_for('index'))
-#         return render_template('submit.html',  title='Submit New Entry', form=form)
-#     return render_template('notloggedin.html')
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user is None or not user.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-#         login_user(user, remember=form.remember_me.data)
-#         next_page = request.args.get('next')
-#         if not next_page or url_parse(next_page).netloc != '':
-#             next_page = url_for('index')
-#         return redirect(next_page)
-#     return render_template('login.html', title='Sign In', form=form)
-
-
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
-
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, email=form.email.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('login'))
-# return render_template('register.html', title='Register', form=form)
+    return render_template('success.html', title='Result', result=result)
